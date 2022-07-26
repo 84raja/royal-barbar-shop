7fae86e415e090c378a23bd2cf18b0aa0cd71114
84raja <d3k.84raja@gmail.com>
2022-07-26 11:26:47 +0700

    change view

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", authRouter);
+app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/auth", authRouter);
 app.use("/billing", billingRouter);
